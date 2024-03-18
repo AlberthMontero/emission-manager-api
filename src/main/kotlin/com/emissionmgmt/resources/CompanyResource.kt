e3d0b1135343f18a616653d75188065f46fbbcea
Alberth Montero <alberthm@gmail.com>
2024-03-18 21:08:12 +0100

    feat: add get request to find all companies

--- src/main/kotlin/com/emissionmgmt/resources/CompanyResource.kt
+++ src/main/kotlin/com/emissionmgmt/resources/CompanyResource.kt
@@ -1,14 +1,16 @@
 package com.emissionmgmt.resources
 
-import jakarta.ws.rs.GET
-import jakarta.ws.rs.Path
-import jakarta.ws.rs.Produces
+import jakarta.ws.rs.*
 import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import jakarta.transaction.Transactional
+
 
 @Path("/companies")
-class CompanyResource {
+class CompanyResource(val repository: CompanyRepository) {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello from RESTEasy Reactive"
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getCompanies(): Response = Response.ok(repository.listAll()).build()
+
 }
