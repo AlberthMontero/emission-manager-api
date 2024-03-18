58891742289d1d45bfe02ebd935efc16e3ec26f5
Alberth Montero <alberthm@gmail.com>
2024-03-18 20:51:28 +0100

    feat: create company repository

---
+++ src/main/kotlin/com/emissionmgmt/resources/CompanyRepository.kt
@@ -0,0 +1,11 @@
+package com.emissionmgmt.resources 
+
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheRepository
+import jakarta.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class CompanyRepository : PanacheRepository<Company> {
+
+    // Define custom query methods here (optional)
+   
+}
